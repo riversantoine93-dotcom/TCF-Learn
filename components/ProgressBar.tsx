2e638fe06059d6586f4e26cebf1eeaad591bce2d
riversantoine93-dotcom <rivers.antoine93@gmail.com>
2026-08-30 18:14:59 -0400

    Delete components directory

--- components/ProgressBar.tsx
+++
@@ -1,4 +0,0 @@
-export default function ProgressBar({ value }: { value: number }) {
-  const safe = Math.max(0, Math.min(100, value));
-  return <div className="progress" aria-label={`${safe}% complete`}><span style={{ width: `${safe}%` }} /></div>;
-}
