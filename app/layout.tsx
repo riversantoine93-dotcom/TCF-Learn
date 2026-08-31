b80f3e98fb8ee4607ab4cddce338bf26788643c8
riversantoine93-dotcom <rivers.antoine93@gmail.com>
2026-08-31 11:25:18 -0400

    Load responsive course lesson styling

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import "./course-layout.css";
 import { AuthProvider } from "@/components/AuthProvider";
 
 export const metadata: Metadata = {
