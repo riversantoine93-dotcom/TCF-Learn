12ceb2142b8384d9cb1f57c56754459656b2d72e
riversantoine93-dotcom <rivers.antoine93@gmail.com>
2026-09-01 11:31:23 -0400

    Pass module slug to sequential access gate

--- app/course/turning-forward/[module]/layout.tsx
+++ app/course/turning-forward/[module]/layout.tsx
@@ -1,5 +1,9 @@
+"use client";
+
+import { useParams } from "next/navigation";
 import PaidCourseGate from "@/components/PaidCourseGate";
 
 export default function ModuleLayout({ children }: { children: React.ReactNode }) {
-  return <PaidCourseGate>{children}</PaidCourseGate>;
+  const params=useParams<{module:string}>();
+  return <PaidCourseGate moduleSlug={params.module}>{children}</PaidCourseGate>;
 }
