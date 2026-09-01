ddf819b0c67d1f034036c867d8796aaa623f695f
riversantoine93-dotcom <rivers.antoine93@gmail.com>
2026-09-01 13:26:45 -0400

    Gate all modules behind required opener videos

--- app/course/turning-forward/[module]/layout.tsx
+++ app/course/turning-forward/[module]/layout.tsx
@@ -2,8 +2,9 @@
 
 import { useParams } from "next/navigation";
 import PaidCourseGate from "@/components/PaidCourseGate";
+import ModuleOpenerGate from "@/components/ModuleOpenerGate";
 
 export default function ModuleLayout({ children }: { children: React.ReactNode }) {
   const params=useParams<{module:string}>();
-  return <PaidCourseGate moduleSlug={params.module}>{children}</PaidCourseGate>;
+  return <PaidCourseGate moduleSlug={params.module}><ModuleOpenerGate moduleSlug={params.module}>{children}</ModuleOpenerGate></PaidCourseGate>;
 }
